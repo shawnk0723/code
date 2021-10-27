b329cdf993c3e6d7e35daa0ac7f125acb2ad85b4
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-10-27 12:34:56 -0700

    Time: 92 ms (86.86%), Space: 45.3 MB (87.25%) - LeetHub

--- intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -16,17 +16,17 @@ var getIntersectionNode = function(headA, headB) {
     let p2 = headB;
     
     while (p1 !== p2) {
-        p1 = p1.next;
-        p2 = p2.next;
-        
-        if (p1 === p2) {
-            return p1;
-        }
+   
+           
         if (p1 === null){
             p1 = headB;
+        }else {
+            p1 = p1.next;
         }
         if (p2 === null) {
             p2 = headA;
+        }else {
+            p2 = p2.next;
         }
     }
     return p1;
