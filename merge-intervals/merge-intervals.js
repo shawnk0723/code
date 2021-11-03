a185a81bfa5147e27752680bf4a10db071eed4b5
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-11-02 20:20:00 -0700

    Time: 84 ms (80.32%), Space: 41.1 MB (38.31%) - LeetHub

---
+++ merge-intervals/merge-intervals.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    intervals.sort((a,b) => a[0] -b[0]);
+    let tmp = [intervals[0]];
+    for (let i = 1; i < intervals.length; i++){
+        [start, end] = intervals[i];
+        if (tmp[tmp.length-1][1] >= start){
+            tmp[tmp.length-1][1] = Math.max(tmp[tmp.length-1][1], end)
+        }else {
+            tmp.push(intervals[i])
+        }
+    }
+    return tmp;
+};
