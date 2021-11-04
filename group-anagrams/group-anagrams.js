e8cc89f6eb38eb9c4254734c75556d5ece312199
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-11-04 04:17:26 -0700

    Time: 132 ms (77.49%), Space: 49.8 MB (68.46%) - LeetHub

--- group-anagrams/group-anagrams.js
+++ group-anagrams/group-anagrams.js
@@ -2,15 +2,32 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function(strs) {
-    const anagrams = {};
-    const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101];
-    for (const word of strs){
-        let key = word.split('').reduce((total, char) => total*primes[char.charCodeAt()-97],1);
-        (!anagrams[key])? anagrams[key] = [word] : anagrams[key].push(word);
+// var groupAnagrams = function(strs) {
+//     const anagrams = {};
+//     const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101];
+//     for (const word of strs){
+//         let key = word.split('').reduce((total, char) => total*primes[char.charCodeAt()-97],1);
+//         (!anagrams[key])? anagrams[key] = [word] : anagrams[key].push(word);
+//     }
+//     return Object.values(anagrams)
+// };
+// //time: O(wn)
+// //space: O(wn)
+
+var groupAnagrams = function(strs){
+    const map ={};
+    
+    for (let i = 0; i < strs.length; i++){
+        const key = strs[i].split('').sort().join('');
+        
+        if (!map[key]) {
+            map[key] = [strs[i]];
+        }else{
+            map[key].push(strs[i])
+        }
     }
-    return Object.values(anagrams)
-};
-//time: O(wn)
-//space: O(wn)
+    return Object.values(map);
+}
+//time: O(n)
+//space: O(n)
 
