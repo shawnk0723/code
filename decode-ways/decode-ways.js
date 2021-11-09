16f4a3131a0e0db8d1860db97533082935a2f3b6
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-11-09 03:53:47 -0800

    Time: 84 ms (58.59%), Space: 40.3 MB (60.91%) - LeetHub

---
+++ decode-ways/decode-ways.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function(s) {
+    if (s == null || s.length == 0) return 0;
+    if (s[0] === '0') return 0;
+    
+    //+1 considering there's empty string
+    const dp = new Array(s.length +1).fill(0);
+    
+    dp[0]=1;
+    dp[1]=1;
+    
+    for (let i =2; i<= s.length;i++){
+        const oneD = Number(s.slice(i-1,i));
+        const twoD = Number(s.slice(i-2, i));
+        if( oneD >= 1){
+            dp[i] += dp[i-1];
+        }
+        if(twoD >= 10 && twoD <= 26){
+            dp[i] += dp[i-2];
+        }
+        //console.log(oneD, twoD)
+    }
+    return dp[s.length]; 
+    
+};
+
+//time:O(n);
+//space:O(n)
