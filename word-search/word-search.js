c7c2e2b3aad97f21a3601d7d5f442c77641c32cb
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-11-10 03:33:53 -0800

    Time: 344 ms (72.90%), Space: 38.8 MB (92.29%) - LeetHub

---
+++ word-search/word-search.js
@@ -0,0 +1,26 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+    const ROWS = board.length, COLS = board[0].length;
+    
+    function dfs(r, c, i){
+        if (i === word.length)return true;
+        if (r<0 || r>=ROWS || c<0 || c>=COLS || word[i] !== board[r][c])return false;
+        
+        board[r][c] = 0;
+        if(dfs(r+1,c,i+1)||dfs(r-1,c,i+1)||dfs(r,c+1,i+1)||dfs(r,c-1,i+1))return true;
+        board[r][c]=word[i];
+    }
+    for(let r = 0; r < ROWS; r++){
+        for(let c=0; c < COLS; c++){
+            if (dfs(r,c,0)) return true
+        }
+    }
+    return false;
+};
+
+//time:O(mn*4^w)
+//space: O(1)
