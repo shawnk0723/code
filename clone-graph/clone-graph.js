762baf2cd5e2ccd880f229d373d3bad23754c19f
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-11-16 04:29:33 -0800

    Time: 76 ms (79.50%), Space: 40.2 MB (45.74%) - LeetHub

--- clone-graph/clone-graph.js
+++ clone-graph/clone-graph.js
@@ -10,19 +10,43 @@
  * @param {Node} node
  * @return {Node}
  */
-function cloneGraph(graph) {
-  var map = {};
-  return traverse(graph);
 
-  function traverse(node) {
-    if (!node) return node;
-    if (!map[node.val]) {
-      map[node.val] = new Node(node.val);
-      map[node.val].neighbors = node.neighbors.map(traverse);
-    }
-    return map[node.val];
-  }
-}
+//dfs
+// function cloneGraph(graph) {
+//   var map = {};
+//   return traverse(graph);
+
+//   function traverse(node) {
+//     if (!node) return node;
+//     if (!map[node.val]) {
+//       map[node.val] = new Node(node.val);
+//       map[node.val].neighbors = node.neighbors.map(traverse);
+//     }
+//     return map[node.val];
+//   }
+// }
 
 //time: O(n)
 //space: O(n)
+
+//bfs
+function cloneGraph(graph){
+    let start = graph;
+    if (start == null)return null;
+    const map = new Map();
+    const queue = [start];
+    map.set(start, new Node(start.val))
+    
+    while (queue.length > 0){
+        const cur = queue.shift();
+        // console.log('q:',cur)
+        for (const neighbor of cur.neighbors){
+            if (!map.has(neighbor)){
+                map.set(neighbor, new Node(neighbor.val))
+                queue.push(neighbor);
+            }
+            map.get(cur).neighbors.push(map.get(neighbor))
+        }
+    }
+    return map.get(start)
+}
