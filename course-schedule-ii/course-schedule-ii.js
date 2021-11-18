15da8317e93fb71f34809f275aac33b087e7e4b3
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-11-18 07:11:08 -0800

    Time: 88 ms (88.10%), Space: 43.2 MB (81.05%) - LeetHub

---
+++ course-schedule-ii/course-schedule-ii.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {number[]}
+ */
+var findOrder = function(numCourses, prerequisites) {
+    
+    const order = [];
+    const zeroDegree = [];
+    const graph = new Map();
+    const indegree = Array(numCourses).fill(0);
+    
+    for (const [e,v] of prerequisites){
+        if (graph.has(v)){
+            graph.get(v).push(e)
+        }else{
+            graph.set(v, [e]);
+        }
+        indegree[e]++;
+    }
+    for(let i=0; i<indegree.length;i++){
+        if(indegree[i]==0)zeroDegree.push(i)
+    }
+    
+    while(zeroDegree.length){
+        const v = zeroDegree.shift();
+        if(graph.has(v)){
+            for(const e of graph.get(v)){
+                indegree[e]--;
+                if(indegree[e]===0)zeroDegree.push(e);
+            }
+        }
+        order.push(v)
+    }
+    return numCourses === order.length ? order : []
+};
+
+//time O(n)
+//space O(n)
