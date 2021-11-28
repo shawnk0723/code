c5d5813e9543ea4c7a592ca1306c4156cdf006dd
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-11-28 07:25:24 -0800

    Time: 88 ms (93.51%), Space: 44.8 MB (53.90%) - LeetHub

---
+++ encode-and-decode-strings/encode-and-decode-strings.js
@@ -0,0 +1,41 @@
+/**
+ * Encodes a list of strings to a single string.
+ *
+ * @param {string[]} strs
+ * @return {string}
+ */
+var encode = function(strs) {
+    let res = "";
+    for (let s of strs){
+        res += s.length.toString() + "#" + s
+    }
+    return res;
+};
+
+/**
+ * Decodes a single string to a list of strings.
+ *
+ * @param {string} s
+ * @return {string[]}
+ */
+var decode = function(s) {
+    let res =[], i=0;
+    while (i < s.length){
+        let j = i
+        while (s[j] !=='#'){
+            j+=1
+        }
+        let length = parseInt(s.slice(i,j))
+        res.push(s.slice(j+1, j+1+length))
+        i = j+1+length;
+        
+    }
+    return res;
+};
+//time O(n)
+//space O(n)
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(strs));
+ */
