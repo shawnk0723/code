bbb979c7127530277abc6148dbd39ce6927ce6f8
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-01 05:41:55 -0800

    Time: 88 ms (65.41%), Space: 40.4 MB (48.95%) - LeetHub

---
+++ palindromic-substrings/palindromic-substrings.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function(s) {
+    let cnt = 0;
+    for (let i=0; i<s.length;i++){
+        helper(s, i, i);//odd pal
+        helper(s, i, i+1);//even pal
+    }
+    return cnt;
+    
+    function helper(s, low, high){
+        while(low>=0 && high <=s.length && s[low]===s[high]){
+            cnt++;
+            low--;
+            high++;
+        }
+    }
+};
+
+//time O(n^2)
+//space O(1)
