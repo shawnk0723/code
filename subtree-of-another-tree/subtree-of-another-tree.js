9462a3c036eef8cbce728846ab8c8e4cf711cbe3
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-01 05:23:54 -0800

    Time: 100 ms (59.68%), Space: 45.8 MB (46.19%) - LeetHub

---
+++ subtree-of-another-tree/subtree-of-another-tree.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function(root, subRoot) {
+    if (!root)return !subRoot;
+    return isEqual(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        
+    }
+    function isEqual(s,t){
+        if(!s || !t)return !s&&!t;
+        if(s.val !== t.val)return false;
+        return isEqual(s.left, t.left)&& isEqual(s.right, t.right)
+    }
+
+
+//time O(n)
+//space O(1)
+
+//trick
+// var isSubtree = function(s, t) {
+//     return JSON.stringify(s).indexOf(JSON.stringify(t))!==-1
+// };
+
