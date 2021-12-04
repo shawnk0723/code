46db78ca9d7e94c343912397e757be7d10e65804
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-04 07:27:04 -0800

    Time: 85 ms (32.72%), Space: 38.5 MB (91.73%) - LeetHub

--- rotate-image/rotate-image.js
+++ rotate-image/rotate-image.js
@@ -2,22 +2,36 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var rotate = function(matrix) {
-    let l = 0, r = matrix.length -1;
+
+
+
+
+var rotate = function(matrix){
+    let l = 0, r = matrix.length-1;
     
-    while (l < r) {
-        for ( let i = 0; i < r-l; i++){
+    while (l < r){
+        for (let i = 0; i < r-l; i++){
             let top = l, bottom = r;
+            
+            //save the topLeft
             let topLeft = matrix[top][l+i]
-            matrix[top][l+i] = matrix[bottom -i][l]
-            matrix[bottom-i][l] = matrix[bottom][r-i]
-            matrix[bottom][r-i] = matrix[top+i][r]
-            matrix[top+i][r] = topLeft
+            
+            //move bottom left into top left
+            matrix[top][l+i] = matrix[bottom-i][l]
+            
+            //move bottom right into bottom left
+            matrix[bottom -i][l] = matrix[bottom][r-i]
+            
+            //move top right into bottom right
+            matrix[bottom][r-i] = matrix[top+i][r];
+            
+            //move top left into top right
+            matrix[top+i][r] = topLeft;
         }
-        r--
-        l++
+        r--;
+        l++;
     }
-};
+}
 
 //time: O(n^2)
 //space: O(1)
