7f12a5b52de36ab90f3741ed5311184977dc5000
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-04 06:17:41 -0800

    Time: 135 ms (21.78%), Space: 48.6 MB (69.02%) - LeetHub

--- maximum-subarray/maximum-subarray.js
+++ maximum-subarray/maximum-subarray.js
@@ -2,17 +2,19 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
+
+
+
+var maxSubArray = function(nums){
     let maxHere = nums[0];
     let maxSofar = nums[0];
     
-    for (let i = 1; i < nums.length; i++){
-        maxHere = Math.max(nums[i], nums[i]+maxHere)   
+    for (let i = 1; i < nums.length;i++){
+        maxHere = Math.max(maxHere+nums[i], nums[i]);
         maxSofar = Math.max(maxSofar, maxHere);
     }
     return maxSofar;
-
-};
+}
 
 //time: O(n)
 //space: O(1)
