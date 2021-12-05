a54d60554da08760869dc95e05ae243b815b8d3e
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-05 04:18:57 -0800

    Time: 72 ms (68.75%), Space: 38.8 MB (21.65%) - LeetHub

--- spiral-matrix/spiral-matrix.js
+++ spiral-matrix/spiral-matrix.js
@@ -2,7 +2,8 @@
  * @param {number[][]} matrix
  * @return {number[]}
  */
-var spiralOrder = function(matrix) {
+
+var spiralOrder = function(matrix){
     let res = [];
     let startRow = 0;
     let endRow = matrix.length -1;
@@ -10,20 +11,20 @@ var spiralOrder = function(matrix) {
     let endCol = matrix[0].length -1;
     
     while (startRow <= endRow && startCol <= endCol) {
-        //top row
-        for (let col = startCol; col <= endCol; col++ ) {
+        //topRow
+        for (let col = startCol; col <= endCol; col++){
             res.push(matrix[startRow][col]);
         }
-        //right col
-        for (let row = startRow+1; row <= endRow; row++) {
+        //rightCol
+        for (let row = startRow+1; row <= endRow; row++){
             res.push(matrix[row][endCol]);
         }
-        //bottom row
-        for (let col = endCol-1; col >= startCol; col--) {
-            if (startRow === endRow )break;
+        //bottomRow
+        for (let col = endCol -1; col >= startCol; col--){
+            if (startRow === endRow)break;
             res.push(matrix[endRow][col])
         }
-        //left col
+        //leftCol
         for (let row = endRow-1; row > startRow; row--){
             if (startCol === endCol)break;
             res.push(matrix[row][startCol])
@@ -31,10 +32,9 @@ var spiralOrder = function(matrix) {
         startRow++;
         endRow--;
         startCol++;
-        endCol--;
+        endCol--;                    
     }
     return res;
-};
-
+}
 //time O(m*n)
 //space O(m*n)
