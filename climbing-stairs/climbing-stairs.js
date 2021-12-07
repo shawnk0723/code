dc90495e6e465a8abd09cbf7ed62afb3a1256f27
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-07 03:57:32 -0800

    Time: 107 ms (11.34%), Space: 38.4 MB (76.52%) - LeetHub

--- climbing-stairs/climbing-stairs.js
+++ climbing-stairs/climbing-stairs.js
@@ -9,10 +9,13 @@ var climbStairs = function(n) {
     let dp = [];
     dp[1]=1;
     dp[2]=2;
+    dp[3]=3;
     
-    for (let i=3; i <=n;i++){
+    for (let i=4; i <=n;i++){
         dp[i]=dp[i-1]+dp[i-2];
     }
     return dp[n];
 };
 
+//time O(n)
+//space O(n)
