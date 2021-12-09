e9e036679fa0dc02e2dda4f5c0580acbc85a113d
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-09 03:35:56 -0800

    Time: 80 ms (67.32%), Space: 40.6 MB (68.55%) - LeetHub

--- binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
+++ binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
@@ -10,22 +10,26 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var levelOrder = function(root) {
-    if(!root)return [];
+var levelOrder = function(root){
+    if (!root)return [];
     let res = [], q = [root];
+    
     while(q[0]){
         let level = [];
-        qLen = q.length;
-        for(let i=0; i < qLen; i++){
+        qLen = q.length
+        for (let i = 0; i < qLen;i++){
             let node = q.shift();
-            level.push(node.val);
+        // console.log(node)
+            
+            level.push(node.val)
             if(node.left)q.push(node.left)
-            if(node.right)q.push(node.right)
+            if(node.right)q.push(node.right);
         }
     res.push(level)
     }
     return res;
-};
+}
+
 
 //time: O(n)
 //space: O(n)
