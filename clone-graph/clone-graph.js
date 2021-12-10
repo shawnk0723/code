288eda61742c9e333239b65a47da0cfbc2c7e7bd
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-10 08:18:19 -0800

    Time: 84 ms (42.01%), Space: 40 MB (68.95%) - LeetHub

--- clone-graph/clone-graph.js
+++ clone-graph/clone-graph.js
@@ -10,43 +10,59 @@
  * @param {Node} node
  * @return {Node}
  */
+function cloneGraph(graph){
+    let map = {};
+    function dfs(node){
+        if (!node)return node;
+        if (!map[node.val]){
+            map[node.val] = new Node(node.val);
+            map[node.val].neighbors = node.neighbors.map(dfs)
+        }
+        return map[node.val]
+    }
+    return dfs(graph)
+}
+//time: O(e+v)//#edges+vertex
+//space: O(e+v)
 
 //dfs
 // function cloneGraph(graph) {
 //   var map = {};
-//   return traverse(graph);
 
-//   function traverse(node) {
+//   function dfs(node) {
 //     if (!node) return node;
 //     if (!map[node.val]) {
 //       map[node.val] = new Node(node.val);
-//       map[node.val].neighbors = node.neighbors.map(traverse);
+//       for (let n of node.neighbors){
+//           map[node.val].neighbors.push(dfs(n))
+//       }  
+//       // map[node.val].neighbors = node.neighbors.map(dfs);
 //     }
 //     return map[node.val];
 //   }
+// return dfs(graph);
 // }
 
-//time: O(n)
-//space: O(n)
+
 
 //bfs
-function cloneGraph(graph){
-    let start = graph;
-    if (start == null)return null;
-    const map = new Map();
-    const queue = [start];
-    map.set(start, new Node(start.val))
+// function cloneGraph(graph){
+//     let start = graph;
+//     if (start == null)return null;
+//     const map = new Map();
+//     const queue = [start];
+//     map.set(start, new Node(start.val))
     
-    while (queue.length > 0){
-        const cur = queue.shift();
-        // console.log('q:',cur)
-        for (const neighbor of cur.neighbors){
-            if (!map.has(neighbor)){
-                map.set(neighbor, new Node(neighbor.val))
-                queue.push(neighbor);
-            }
-            map.get(cur).neighbors.push(map.get(neighbor))
-        }
-    }
-    return map.get(start)
-}
+//     while (queue.length > 0){
+//         const cur = queue.shift();
+//         // console.log('q:',cur)
+//         for (const neighbor of cur.neighbors){
+//             if (!map.has(neighbor)){
+//                 map.set(neighbor, new Node(neighbor.val))
+//                 queue.push(neighbor);
+//             }
+//             map.get(cur).neighbors.push(map.get(neighbor))
+//         }
+//     }
+//     return map.get(start)
+// }
