48ce8317fd6ae8ffee98f030cb23f6790370007c
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-10 09:47:46 -0800

    Time: 84 ms (85.18%), Space: 42.8 MB (53.55%) - LeetHub

--- valid-palindrome/valid-palindrome.js
+++ valid-palindrome/valid-palindrome.js
@@ -3,6 +3,14 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+
+// [ and ] are the start and end of a character set.
+// \W means "non-word", as opposed to \w which will match a word.
+// _ is the "_" character.
+// / mark the beginning and end of a regular expression.
+// g means it's a global search.
+//replace them with ''
+    
     var nonAlpha = s.replace(/\W|_/g, '');
     var reversed = nonAlpha.split('').reverse().join('');
     return nonAlpha.toLowerCase() == reversed.toLowerCase();
