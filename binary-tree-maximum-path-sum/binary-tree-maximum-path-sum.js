e432563c7650e98b51e29d9ba5b1406236364ff0
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-10 06:55:48 -0800

    Time: 96 ms (62.31%), Space: 48.5 MB (52.35%) - LeetHub

--- binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.js
+++ binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.js
@@ -10,17 +10,21 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var maxPathSum = function(root) {
-    let max = -Infinity;
-    function pathSum(root){
-        if(!root)return 0;
-        let left = Math.max(0, pathSum(root.left))
-        let right = Math.max(0, pathSum(root.right))
-        max = Math.max(root.val+left+right, max);
-        return Math.max(left, right) + root.val;
+
+var maxPathSum = function(root){
+    let max = -Infinity
+    function dfs(root){
+        if (!root)return 0;
+        let left = Math.max(0, dfs(root.left))
+        let right = Math.max(0, dfs(root.right))
+        //with split
+        max = Math.max(max, root.val + left + right)
+        //without split
+        return Math.max(left, right) + root.val
     }
-    pathSum(root);
-    return max
-}; 
+    dfs(root);
+    return max;
+}
+
 //time O(n)
 //space O(h)
