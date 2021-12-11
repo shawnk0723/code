f0a9e9355845f948368060ebf7f052571a1273d9
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-11 09:57:32 -0800

    Time: 76 ms (93.76%), Space: 41.2 MB (71.27%) - LeetHub

--- linked-list-cycle/linked-list-cycle.js
+++ linked-list-cycle/linked-list-cycle.js
@@ -10,18 +10,21 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+
 var hasCycle = function(head) {
-    if (!head) return false;
+    if (!head)return false;
     
     let slow = head, fast = head.next;
     
-    while (slow !== fast) {
-        if (!fast || !fast.next) return false;
+    while (slow !== fast){
+        if (!fast || !fast.next)return false;
         slow = slow.next;
         fast = fast.next.next;
     }
     return true;
-};
+}
 
 //time: O(n)
 //space: O(1)
+
+
