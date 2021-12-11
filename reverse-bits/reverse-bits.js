b78cc7a728301ef5aba433896314ac2a79aad9df
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-11 09:24:25 -0800

    Time: 88 ms (55.54%), Space: 40.8 MB (19.30%) - LeetHub

--- reverse-bits/reverse-bits.js
+++ reverse-bits/reverse-bits.js
@@ -2,6 +2,8 @@
  * @param {number} n - a positive integer
  * @return {number} - a positive integer
  */
+
+
 var reverseBits = function(n) {
-   return Number.parseInt(n.toString(2).split('').reverse().join('').padEnd(32,'0'),2)
-};
+    return Number.parseInt(n.toString(2).split('').reverse().join('').padEnd(32, '0'),2)
+}
