b75231ed5af3fb3aa4d4df72fd710d2c589d27ae
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-14 09:34:35 -0800

    Time: 180 ms (12.65%), Space: 41.2 MB (98.99%) - LeetHub

--- number-of-islands/number-of-islands.js
+++ number-of-islands/number-of-islands.js
@@ -2,32 +2,31 @@
  * @param {character[][]} grid
  * @return {number}
  */
+
 var numIslands = function(grid) {
-    if(grid===null || grid.length ===0){
-        return 0;
-    }
-    let iNum = 0;
+    if (!grid || grid.length==0)return 0;
+    
+    let inum = 0;
     for (let i = 0; i < grid.length;i++){
-        for (let j= 0; j < grid[i].length;j++){
-            if(grid[i][j] ==='1'){
-                iNum += getIslandCount(grid, i, j);
+        for (let j = 0; j < grid[i].length; j++){
+            if (grid[i][j] === '1'){
+                inum +=gridCount(grid, i, j)
             }
         }
     }
-    return iNum;
-};
+    return inum;
+}
 
-let getIslandCount = function(grid, i, j){
-    if(i <0 || i>=grid.length || j< 0 || j>= grid[i].length || grid[i][j]=== '0')return 0;    
-
-grid[i][j] = '0';
-
-getIslandCount(grid, i+1, j)
-getIslandCount(grid, i-1, j)
-getIslandCount(grid, i, j+1)
-getIslandCount(grid, i, j-1)
-
-return 1;
+function gridCount(grid, i, j){
+    if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] ==="0")return 0;
+    
+    grid[i][j] = '0';
+    gridCount(grid, i+1,j )
+    gridCount(grid, i-1,j )
+    gridCount(grid, i,j+1 )
+    gridCount(grid, i,j-1 )
+    
+    return 1;
 }
 
 //time O(n*m)
