e3824cf4165d73a84ef93d6c05ce2add2fddf227
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-14 03:14:19 -0800

    Time: 109 ms (18.89%), Space: 40 MB (91.60%) - LeetHub

--- number-of-1-bits/number-of-1-bits.js
+++ number-of-1-bits/number-of-1-bits.js
@@ -2,13 +2,19 @@
  * @param {number} n - a positive integer
  * @return {number}
  */
+
 var hammingWeight = function(n) {
     let sum = 0;
     
     while (n != 0){
-        console.log(n)
+        //input in binary is 11(0....1011) so & 1 give 1(1011&0001=>0001)
+        //continue to add until not zero(0000)
         sum += n & 1
+        // console.log(n, sum)
         n = n >>> 1
     }
     return sum;
 };
+
+//time 0(n)
+//space O(1)
