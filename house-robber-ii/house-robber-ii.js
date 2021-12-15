39d34091a56d1c56a6e6f86dea565581d291d590
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-15 05:29:13 -0800

    Time: 80 ms (29.50%), Space: 38.5 MB (85.08%) - LeetHub

--- house-robber-ii/house-robber-ii.js
+++ house-robber-ii/house-robber-ii.js
@@ -2,21 +2,24 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
-    if(nums.length==0)return 0;
-    if(nums.length==1)return nums[0];
+var rob = function(nums){
+    if (nums.length==0)return 0;
+    if (nums.length ==1) return nums[0];
     
-    const robHouse = function(start, end){
+    function robHouse(start, end){
+        
         let prev = 0, cur = 0;
-        for(let i = start; i < end; i++){
-            let tmp = Math.max(nums[i]+prev, cur);
+        for (let i = start; i < end;i++){
+            let tmp = Math.max(prev+nums[i], cur);
             prev = cur;
             cur = tmp
         }
         return cur
-    } 
-    return Math.max(robHouse(0,nums.length-1), robHouse(1,nums.length))
-};
-
+    }
+    return Math.max(robHouse(0, nums.length-1), robHouse(1, nums.length))
+    
+}
 //time O(n)
 //space O(1)
+
+
