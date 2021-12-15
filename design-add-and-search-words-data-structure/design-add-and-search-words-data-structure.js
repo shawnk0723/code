a7d5f44212ace2c70c6cfb74e808877dec0e3759
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-15 08:18:17 -0800

    Time: 216 ms (76.88%), Space: 61 MB (27.99%) - LeetHub

--- design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.js
+++ design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.js
@@ -1,43 +1,80 @@
-
-var WordDictionary = function() {
+var WordDictionary = function(){
     this.trie = {};
-};
+}
 
-/** 
- * @param {string} word
- * @return {void}
- */
-WordDictionary.prototype.addWord = function(word) {
-    let root = this.trie;
-    for (let i=0; i<word.length;i++){
-        if(root[word[i]]==null)root[word[i]] = {};
-        root = root[word[i]]
+WordDictionary.prototype.addWord = function(word){
+    let cur = this.trie;
+    for (let c of word){
+        if (!cur[c])cur[c] = {};
+        cur = cur[c];
     }
-    root.isEnd = true;
+    cur.isEnd = true;
 };
 
-/** 
- * @param {string} word
- * @return {boolean}
- */
-WordDictionary.prototype.search = function(word) {
-    return this.dfs(word, 0, this.trie);
-};
+WordDictionary.prototype.search = function(word){
+  return this.dfs(word, 0, this.trie)
+}
 
 WordDictionary.prototype.dfs = function(word, i, node){
-    if (i === word.length) return node.isEnd == true;
+    if (i === word.length)return node.isEnd === true;
     
-    if (word[i] =='.'){
-        for(let key in node){
-            if(this.dfs(word, i+1, node[key]))return true;
+    if (word[i]==='.'){
+        for (let key in node){
+            if(this.dfs(word,i+1, node[key]))return true;
         }
     }else{
-        if (node[word[i]]!=null){
-            return this.dfs(word, i+1, node[word[i]])
+        if(node[word[i]]){
+            return this.dfs(word,i+1, node[word[i]])
         }
     }
     return false;
 }
+
+
+
+
+
+
+
+// var WordDictionary = function() {
+//     this.trie = {};
+// };
+
+// /** 
+//  * @param {string} word
+//  * @return {void}
+//  */
+// WordDictionary.prototype.addWord = function(word) {
+//     let root = this.trie;
+//     for (let i=0; i<word.length;i++){
+//         if(root[word[i]]==null)root[word[i]] = {};
+//         root = root[word[i]]
+//     }
+//     root.isEnd = true;
+// };
+
+// /** 
+//  * @param {string} word
+//  * @return {boolean}
+//  */
+// WordDictionary.prototype.search = function(word) {
+//     return this.dfs(word, 0, this.trie);
+// };
+
+// WordDictionary.prototype.dfs = function(word, i, node){
+//     if (i === word.length) return node.isEnd == true;
+    
+//     if (word[i] =='.'){
+//         for(let key in node){
+//             if(this.dfs(word, i+1, node[key]))return true;
+//         }
+//     }else{
+//         if (node[word[i]]!=null){
+//             return this.dfs(word, i+1, node[word[i]])
+//         }
+//     }
+//     return false;
+// }
 // add time O(1), space O(n)
 // search time O(n^2), space O(1)
 
