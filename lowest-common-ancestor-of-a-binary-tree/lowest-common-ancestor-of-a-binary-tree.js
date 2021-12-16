112bc248078b414ad1018c103627520f853878ea
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-16 06:11:41 -0800

    Time: 147 ms (13.81%), Space: 48.5 MB (55.48%) - LeetHub

--- lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.js
+++ lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.js
@@ -11,12 +11,14 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
-var lowestCommonAncestor = function(root, p, q) {
-    if(!root || root === p || root === q) return root;
-    let left = lowestCommonAncestor(root.left, p, q);
-    let right = lowestCommonAncestor(root.right,p, q);
-    return (left && right) ? root : (left || right);
-};
+var lowestCommonAncestor = function(root, p, q){
+    if (!root || root === p || root === q)return root;
+    let left = lowestCommonAncestor(root.left, p, q)
+    let right = lowestCommonAncestor(root.right, p, q)
+    return (right && left) ? root : (right || left)
+}
 
-//time O(n)
+
+
+//time O(logn)
 //space O(1)
