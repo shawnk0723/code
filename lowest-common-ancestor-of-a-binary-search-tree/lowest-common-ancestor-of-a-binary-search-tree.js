9a38bdebcdbfb1d55cc8cccde4d2b9ade1b3f3f4
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-16 05:50:17 -0800

    Time: 117 ms (21.06%), Space: 48.5 MB (96.01%) - LeetHub

--- lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.js
+++ lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.js
@@ -12,18 +12,14 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
-var lowestCommonAncestor = function(root, p, q) {
+var lowestCommonAncestor = function(root, p, q){
     let cur = root;
-    
-    while(cur){
-        if (p.val > cur.val && q.val > cur.val){
-            cur = cur.right;
-        }else if (p.val < cur.val && q.val < cur.val){
-            cur = cur.left;
-        }else {
-            return cur;
-        }
+    while (cur){
+        if (cur.val < p.val && cur.val < q.val)cur = cur.right;
+        else if (cur.val > p.val && cur.val > q.val)cur = cur.left;
+        else return cur //either split or equal p/q
     }
-};
+}
 //time: O(logn)
 //space: O(1)
+
