e1e1af6173a09bbec5653b7bb6bda22f2f24ec31
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-16 07:26:01 -0800

    Time: 140 ms (35.88%), Space: 48.1 MB (98.28%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.js
+++ product-of-array-except-self/product-of-array-except-self.js
@@ -2,23 +2,26 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var productExceptSelf = function(nums) {
+//num: 1,2,3,4
+//prefix: 1,2,6,24
+//postfix:24,24,12,4
+//output(exclude i pos and pre * post): 24,12,8,6 
+var productExceptSelf = function(nums){
+    let prefix = 1, postfix =1;
     let res = [];
-    let prefix = 1;
-    let postfix = 1;
     
-    
-    for (let i = nums.length-1; i>=0; i--){
+    for (let i = nums.length-1; i >= 0; i--){
         res[i] = postfix;
         postfix *= nums[i];
     }
-    for (let i =0; i < nums.length;i++){
-        res[i] *= prefix;
-        prefix *= nums[i]
-        
+    for (let i = 0; i < nums.length; i++){
+        // console.log(res[i], prefix)
+        res[i] *=prefix;
+        prefix *= nums[i];
     }
     return res;
-};
+}
+
 
 //time O(n)
 //space O(1)
