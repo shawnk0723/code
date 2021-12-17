f39962b6ae4bf281bb1674b0c1717a53bef02086
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-17 05:10:38 -0800

    Time: 120 ms (16.78%), Space: 41 MB (11.89%) - LeetHub

--- meeting-rooms/meeting-rooms.js
+++ meeting-rooms/meeting-rooms.js
@@ -2,14 +2,17 @@
  * @param {number[][]} intervals
  * @return {boolean}
  */
-var canAttendMeetings = function(intervals) {
-    if(!intervals || intervals === 1)return true;
+var canAttendMeetings = function(intervals){
+    if(!intervals || intervals.length ==1)return true;
     
-    let sorted = intervals.sort((a,b)=> a[0]-b[0]);
-    for (let i=0; i< sorted.length-1; i++){
-        if(sorted[i][1]>sorted[i+1][0])return false;
+    intervals.sort((a,b)=> a[0]-b[0]);
+    
+    for (let i = 0; i < intervals.length-1; i++){
+        if (intervals[i][1] > intervals[i+1][0])return false
     }
     return true;
-};
-//time O(logn)
+}
+
+//time O(nlogn)
 //space O(1)
+
