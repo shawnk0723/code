6f4ee450d81e5f9d7aa05330138c485af72cc844
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-27 22:02:07 -0800

    Time: 88 ms (60.44%), Space: 40.3 MB (98.85%) - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.js
+++ meeting-rooms-ii/meeting-rooms-ii.js
@@ -8,21 +8,63 @@
  * }
  */
 
-var minMeetingRooms = function(intervals) {
-    if (!intervals || intervals.length < 1)return 0;
+// var minMeetingRooms = function(intervals){
+//     if (!intervals || intervals.length < 1) return 0;
     
-    let rooms = 0, end =0;
-    const starts = intervals.map(a=> a[0]).sort((a,b)=> a-b);
-    const ends = intervals.map(a=> a[1]).sort((a,b)=>a-b);
-    for(let i=0; i<intervals.length;i++){
-        if(starts[i] < ends[end]){
-            rooms++;
+//     let rooms = 0, e = 0;
+//     const start = intervals.map(a => a[0]).sort((a,b) => a-b);
+//     const ends = intervals.map(a => a[1]).sort((a,b) => a-b);
+    
+//     for (let i = 0; i < intervals.length; i++){
+//         if (start[i] < ends[e]){
+//             rooms++;
+//         }else{
+//             e++
+//         }
+//     }
+//     return rooms;
+// }
+
+
+var minMeetingRooms = function(intervals){
+    if (intervals.length < 1 || !intervals) return 0;
+    
+    let res = 0, cnt = 0;
+    
+    const start = intervals.map(a => a[0]).sort((a,b) => a-b);
+    const ends = intervals.map(a => a[1]).sort((a,b) => a-b);
+    
+    let s = 0, e = 0;
+    while (s < intervals.length){
+        if (start[s] < ends[e]){
+            s++;
+            cnt++;
         }else{
-            end++;
+            e++;
+            cnt--;
         }
+        res = Math.max(res, cnt);
     }
-    return rooms;
-};
+    return res;
+    
+}
+
+
+// var minMeetingRooms = function(intervals) {
+//     if (!intervals || intervals.length < 1)return 0;
+    
+//     let rooms = 0, end =0;
+//     const starts = intervals.map(a=> a[0]).sort((a,b)=> a-b);
+//     const ends = intervals.map(a=> a[1]).sort((a,b)=>a-b);
+//     for(let i=0; i<intervals.length;i++){
+//         if(starts[i] < ends[end]){
+//             rooms++;
+//         }else{
+//             end++;
+//         }
+//     }
+//     return rooms;
+// };
 
 //time O(nlogn)
 //space O(n)
