01c4ac1e1d00c219ca64a6bd762820032c1cd031
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2021-12-31 00:42:38 -0800

    Time: 127 ms (15.27%), Space: 44.6 MB (66.41%) - LeetHub

--- encode-and-decode-strings/encode-and-decode-strings.js
+++ encode-and-decode-strings/encode-and-decode-strings.js
@@ -3,37 +3,68 @@
  *
  * @param {string[]} strs
  * @return {string}
- */
-var encode = function(strs) {
-    let res = "";
-    for (let s of strs){
-        res += s.length.toString() + "#" + s
-        // console.log(res)
-    }
-    return res;
-};
+//  */
 
-/**
- * Decodes a single string to a list of strings.
- *
- * @param {string} s
- * @return {string[]}
- */
+var encode = function(strs){
+    let res = "";
+    
+    for (let str of strs){
+        res += str.length.toString() + '#' + str;
+    }
+    // console.log(res)
+    return res;
+}
+
 var decode = function(s) {
-    let res =[], i=0;
-    while (i < s.length){
-        let j = i
-        while (s[j] !=='#'){
-            j+=1
-            // console.log(s[j])
+    let res = [];
+    let i = 0;
+    
+    while (s.length > i){
+        let j = i;
+        while (s[j] !== '#'){
+            j++;
         }
-        let length = parseInt(s.slice(i,j))
+        let length = parseInt(s.slice(i, j))
         res.push(s.slice(j+1, j+1+length))
         i = j+1+length;
-        
     }
     return res;
-};
+        
+}
+//time O(n)
+//space 0(n)
+
+// var encode = function(strs) {
+//     let res = "";
+//     for (let s of strs){
+//         res += s.length.toString() + "#" + s
+//         // console.log(res)
+//     }
+//     return res;
+// };
+
+// /**
+//  * Decodes a single string to a list of strings.
+//  *
+//  * @param {string} s
+//  * @return {string[]}
+//  */
+// var decode = function(s) {
+//     let res =[], i=0;
+//     while (i < s.length){
+//         let j = i
+//         while (s[j] !=='#'){
+//             j+=1
+//         }
+//         let length = parseInt(s.slice(i,j))
+//         // console.log(i,j)
+//         res.push(s.slice(j+1, j+1+length))
+//         // console.log(j+1, j+1+length)
+//         i = j+1+length;
+        
+//     }
+//     return res;
+// };
 //time O(n)
 //space O(n)
 
