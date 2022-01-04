4e4d30972e7204cfda4312c0179e478b390ff6e6
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2022-01-04 08:26:27 -0800

    Time: 151 ms (12.60%), Space: 43.4 MB (34.20%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    const freqMap = new Map();
+    const bucket = [];
+    const result = [];
+    
+    for(let num of nums){
+        freqMap.set(num, (freqMap.get(num) || 0)+1)
+    }
+    
+    for(let [key,value] of freqMap){
+        if(typeof bucket[value]==='object'){
+        bucket[value].push(key);
+        }else{
+            bucket[value] = [key];
+        }
+        // console.log(bucket)
+    }
+    
+    for(let i = bucket.length-1; i>=0;i--){
+        if(bucket[i]) result.push(...bucket[i]);
+        if(result.length===k)break;
+    }
+    return result;
+};
+
+//time O(n)
+//space O(n)
