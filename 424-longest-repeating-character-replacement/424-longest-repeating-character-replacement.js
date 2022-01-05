38187d83917a00f6ff50434a70fb4dfb69c8b77b
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2022-01-05 02:28:24 -0800

    Time: 92 ms (70.69%), Space: 40.7 MB (38.10%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
@@ -23,7 +23,23 @@
 // };
 //time O(n)
 //space O(n)
-
+/*
+Maintain left and right pointer, max instances of a single char, and visit counts for each char.
+for each char in string
+increment visit count for this char
+if new visit count is higher than max, update max
+if length of current string without max char count is greater than k,
+then we know the new char made it such that there are more chars missing than can be replaced by k,
+so we will remove the first char
+and increment left pointer
+increment right pointer to look at next char.
+In the end, the answer is whatever the window size is. This is because we never shrink the window size.
+As we look at new chars, we increase the window size.
+Once we see we can no longer increase due to limitation of k, we slide the window forward.
+In these inbetween states, it's possible the window doesn't span a valid subset,
+but that doesn't matter because the window size at one point did span a valid set.
+Instead, we wait until there's a possibility of a better set, which is when there is a substring with more instances of some char.
+*/
 
 var characterReplacement = function(s, k){
     let l = 0, r = 0;
@@ -45,3 +61,6 @@ var characterReplacement = function(s, k){
 }
 //time O(n); or O(26)
 //space O(n) or O(26)
+
+
+
