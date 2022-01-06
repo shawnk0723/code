296f7f847e311a0aa33948154761a2fef704538e
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2022-01-06 02:23:06 -0800

    Time: 287 ms (41.17%), Space: 71.6 MB (11.51%) - LeetHub

---
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+// var eraseOverlapIntervals = function(intervals) {
+//     intervals.sort((a,b)=> a[1] - b[1]);
+//     let prev = intervals[0], remove = 0;
+//     for (let i = 1; i < intervals.length;i++){
+//         if(intervals[i][0] < prev[1])remove++;
+//         else prev = intervals[i];
+//     }
+//     return remove;
+// };
+
+var eraseOverlapIntervals = function(intervals){
+    intervals.sort((a,b)=> a[1]-b[1]);
+    
+    let prev = intervals[0], remove = 0;
+    for (let i = 1; i < intervals.length;i++){
+        if(intervals[i][0]<prev[1])remove++;
+        else prev = intervals[i];
+    }
+    return remove;
+}
+
+
+//time O(nlogn)
+//space O(1)
