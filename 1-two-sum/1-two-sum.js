4a960b5bae4a757ed9e378de8af074586b43773f
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2022-01-08 05:02:05 -0800

    Time: 107 ms (54.82%), Space: 40.7 MB (36.14%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+// var twoSum = function(nums, target) {
+//     let map = new Map();
+    
+//     for (let i = 0; i < nums.length; i++) {
+//         let num1 = nums[i];
+//         let num2 = target - num1;
+//         if (map.has(num2) ){
+//             return [i, map.get(num2)]
+//         }
+//         map.set(num1, i)
+//     }
+    
+    
+// };
+
+// var twoSum = function(nums, target){
+//     let map = new Map();
+    
+//     for (let i = 0; i < nums.length; i++){
+//         let num1 = nums[i];
+//         let num2 = target - num1;
+//         if (map.has(num2)) return [i, map.get(num2)]
+//         map.set(num1, i)
+//     }
+    
+// }
+//time O(n)
+//space O(n)
+
+var twoSum = function(nums, target){
+    let map = new Map();
+    
+    for(let i = 0; i < nums.length; i++){
+        let need = target - nums[i];
+        
+        if (map.has(need))return [i, map.get(need)]
+        map.set(nums[i], i);
+    }
+}
