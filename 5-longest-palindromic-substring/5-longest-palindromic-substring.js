7eb5393ce87c6e757318fe48603a51375646bc17
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2022-01-09 07:47:33 -0800

    Time: 296 ms (37.42%), Space: 41.2 MB (84.80%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s){
+    if (s.length < 1 || s === null) return "";
+    
+    let longest = "";
+    
+    for (let i = 0; i < s.length; i++){
+        let even = expendFromCenter(s, i+1, i);
+        let odd = expendFromCenter(s, i, i);
+        
+        if (longest.length < even.length)longest = even;
+        if (longest.length < odd.length)longest = odd;
+    }
+    return longest;
+}
+
+function expendFromCenter(s, l, r){
+    let i = 0;
+    while (s[l-i] && s[l-i] === s[r+i]){
+        i++;
+    }
+    i--;
+    return s.slice(l-i, r+i+1)
+}
