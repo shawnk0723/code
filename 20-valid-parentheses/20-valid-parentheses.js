53521e4c665fee12c139171014073f925ff833a5
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2022-01-10 05:31:49 -0800

    Time: 64 ms (98.30%), Space: 40.1 MB (35.60%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s){
+    let closing = {')':'(', '}':'{', ']':'['};
+    let stack = [];
+    
+    for (let ch of s) {
+        if (ch in closing){
+            if (stack && closing[ch] === stack[stack.length-1]){
+                stack.pop();
+            }else{
+                return false;
+            }
+            
+        }else {
+            stack.push(ch);
+        }
+    }
+    return stack.length === 0;
+}
+
+//time O(n)
+//space O(n)
