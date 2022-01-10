dbd0fc42e9b03e2babb11142c8ff7962c1a12620
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2022-01-10 07:12:13 -0800

    Time: 164 ms (13.56%), Space: 48.7 MB (76.25%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    var min = prices[0]; 
+    var min = Number.MAX_SAFE_INTEGER; 
     var max = 0;
     for (var i = 0; i < prices.length; i++) {
         min = Math.min(min, prices[i]);
