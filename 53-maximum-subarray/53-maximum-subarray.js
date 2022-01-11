a7ffc350f943051072893f5dabbbcb02325b21f8
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2022-01-11 13:36:20 -0800

    Time: 166 ms (10.33%), Space: 48.4 MB (93.11%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -2,6 +2,11 @@
  * @param {number[]} nums
  * @return {number}
  */
+/*
+kadane algo
+max = Math.max(max, current) means that we choose only the highest value (previous max or new current).
+current = Math.max(current, 0) means that if the value was negative, then we throw it away and set zero as default. Why? Because it makes no sense to summarize the negative values, they simply do not interest us.
+*/
 var maxSubArray = function(nums){
     let maxHere = 0;
     let maxSofar = -Number.MAX_VALUE;
