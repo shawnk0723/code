a3217d763b2bcc9269abc45077b441868dc85161
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2022-01-12 23:08:43 -0800

    Time: 141 ms (7.50%), Space: 38.9 MB (97.03%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if(!strs.length) return '';
+    
+    for(let i = 0; i < strs[0].length; i++) {
+        for(let j =1; j < strs.length; j++) {
+            // console.log(strs[j][i], strs[0][i])
+            
+            if(strs[j][i] !== strs[0][i]) {
+               return strs[0].slice(0, i); 
+            }
+        }
+    }
+    return strs[0];
+};
+
+//time O(n)
+//space O(1)
