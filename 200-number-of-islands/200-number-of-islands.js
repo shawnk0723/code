58ffa7b8e434204c0ad6dcb65d4af9f4cf559375
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2022-01-20 21:07:05 -0800

    Time: 124 ms (44.98%), Space: 41.9 MB (64.20%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.js
@@ -0,0 +1,61 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+
+// var numIslands = function(grid) {
+//     if (!grid || grid.length==0)return 0;
+    
+//     let inum = 0;
+//     for (let i = 0; i < grid.length;i++){
+//         for (let j = 0; j < grid[i].length; j++){
+//             if (grid[i][j] === '1'){
+//                 inum +=gridCount(grid, i, j)
+//             }
+//         }
+//     }
+//     return inum;
+// }
+
+// function gridCount(grid, i, j){
+//     if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] ==="0")return 0;
+    
+//     grid[i][j] = '0';
+//     gridCount(grid, i+1,j )
+//     gridCount(grid, i-1,j )
+//     gridCount(grid, i,j+1 )
+//     gridCount(grid, i,j-1 )
+    
+//     return 1;
+// }
+
+//time O(n*m)
+//space O(1)
+
+var numIslands = function(grid){
+    if (!grid || grid.length == 0)return 0;
+    
+    let iNum = 0;
+    for (let i = 0; i < grid.length;i++){
+        for (let j = 0; j < grid[i].length; j++){
+            if (grid[i][j] == '1'){
+                iNum += gridCount(grid, i, j)
+            }
+        }
+    }
+    return iNum;
+}
+
+function gridCount (grid, i, j){
+    if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] === '0')return 0;
+    
+    grid[i][j] = '0';
+    gridCount(grid, i+1, j)
+    gridCount(grid, i-1, j)
+    gridCount(grid, i, j+1)
+    gridCount(grid, i, j-1)
+    return 1;
+}
+
+//time O(n*m);
+//space 0(1)
