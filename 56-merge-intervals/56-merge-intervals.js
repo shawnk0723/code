7124c8f716ae3900afcb83bfbf02efd6aeb4e2ef
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2022-01-24 04:16:43 -0800

    Time: 112 ms (50.37%), Space: 45.3 MB (42.66%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -3,18 +3,32 @@
  * @return {number[][]}
  */
 
+// var merge = function(intervals){
+//     intervals.sort((a,b)=> a[0]-b[0]);
+//     let tmp = [intervals[0]];
+//     for (let i = 1; i < intervals.length;i++){
+//         [start,end] = intervals[i];
+//         if (tmp[tmp.length-1][1]>=start){
+//             tmp[tmp.length-1][1] = Math.max(tmp[tmp.length-1][1], end)
+//         }else{
+//             tmp.push(intervals[i])
+//         }
+//     }
+//     return tmp
+// }
+//time O(nlogn)
+//space O(n)
+
 var merge = function(intervals){
-    intervals.sort((a,b)=> a[0]-b[0]);
+    intervals.sort((a,b) => a[0]-b[0]);
     let tmp = [intervals[0]];
     for (let i = 1; i < intervals.length;i++){
-        [start,end] = intervals[i];
+        [start, end] = intervals[i];
         if (tmp[tmp.length-1][1]>=start){
-            tmp[tmp.length-1][1] = Math.max(tmp[tmp.length-1][1], end)
+            tmp[tmp.length-1][1] = Math.max(tmp[tmp.length-1][1], end);
         }else{
             tmp.push(intervals[i])
         }
     }
-    return tmp
+    return tmp;
 }
-//time O(nlogn)
-//space O(n)
