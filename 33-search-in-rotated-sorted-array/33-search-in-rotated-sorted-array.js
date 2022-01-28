931c313df554851ef2643c272c0c3ebc3cc84105
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2022-01-28 01:15:41 -0800

    Time: 128 ms (12.52%), Space: 42.3 MB (6.10%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target){
+    let l = 0, r = nums.length -1;
+    
+    while (l <= r) {
+        let mid = Math.floor((l+r)/2);
+        if (nums[mid] === target)return mid;
+        
+        //divid into two. one should be sorted
+        //check if left side is sorted
+        
+        if (nums[l] <= nums[mid]){
+            if (nums[l] <= target && nums[mid] >= target) {
+            //target is in the left
+                r = mid -1;
+            }else {
+            //target in the right
+                l = mid +1;
+            }
+            //check if right sorted
+        }else {
+            if (nums[mid] <= target && nums[r] >= target){
+                l = mid +1;
+            }else {
+                r = mid -1;
+            }
+        }
+    }
+    return -1;
+}
+
+//time O(logn)
+//space O(1)
+
