092b6a5e1ff7d3a187c1db2c1fe1584744a0592f
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2022-02-08 00:18:32 -0800

    Time: 119 ms (59.81%), Space: 46.2 MB (25.64%) - LeetHub

---
+++ 55-jump-game/55-jump-game.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+var canJump = function(nums){
+    let goal = nums.length-1;
+    
+    for (let i = nums.length-1; i >= 0; i--){
+        if (nums[i]+i >= goal){
+            goal = i;
+        }
+    }
+    return goal === 0;
+}
+//time: O(n)
+//space: O(1)
