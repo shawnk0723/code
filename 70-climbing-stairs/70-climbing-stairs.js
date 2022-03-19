92fd11e588cdf4ebd611b33af3428a223a16475e
shawnk0723 <40378909+shawnk0723@users.noreply.github.com>
2022-03-19 04:16:40 -0700

    Time: 102 ms (19.41%), Space: 41.6 MB (70.95%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    
+    if (n<=3) return n;
+    
+    let dp = [];
+    dp[1]=1;
+    dp[2]=2;
+    dp[3]=3;
+    
+    for (let i=4; i <=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+    return dp[n];
+};
+//time O(n)
+//space O(n)
